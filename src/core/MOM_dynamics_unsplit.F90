3125feda96d13a831294727373c0e35b7651f5cc
Joseph Ansong <jkansong@umich.edu>
2015-06-15 13:32:35 -0400

    testing debug

--- src/core/MOM_dynamics_unsplit.F90
+++ src/core/MOM_dynamics_unsplit.F90
@@ -392,7 +392,8 @@ subroutine step_MOM_dyn_unsplit(u, v, h, tv, visc, Time_local, dt, fluxes, &
  
 !Joe
   if (CS%debug) then
-    call MOM_state_chksum("Ansong 1", up,vp,h_av,dt_pred,G,G%t11)
+!    call MOM_state_chksum("Predictor 1", up, vp, h_av, uh, vh, G)
+    call MOM_state_chksum("Ansong 1", up,vp,h_av,uh,dt_pred,G)
   endif
 !endJoe
 ! Joe
