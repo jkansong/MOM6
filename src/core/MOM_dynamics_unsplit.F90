73ab8619974aa5c80188d287b9235ce297e8acd3
Joseph Ansong <jkansong@umich.edu>
2015-06-15 13:57:58 -0400

    added debug statement to MOM_dynamics_unsplit.F90

--- src/core/MOM_dynamics_unsplit.F90
+++ src/core/MOM_dynamics_unsplit.F90
@@ -390,13 +390,6 @@ subroutine step_MOM_dyn_unsplit(u, v, h, tv, visc, Time_local, dt, fluxes, &
   call cpu_clock_begin(id_clock_pass)
   call pass_vector(up, vp, G%Domain)
  
-!!Joe
-!  if (CS%debug) then
-!!    call MOM_state_chksum("Predictor 1", up, vp, h_av, uh, vh, G)
-!    call MOM_state_chksum("Ansong 1", up,vp,h_av,uh,G)
-!  endif
-!!endJoe
-
 ! Joe
   if (CS%use_topodrag) then
    if (CS%debug) then
@@ -551,18 +544,14 @@ subroutine step_MOM_dyn_unsplit(u, v, h, tv, visc, Time_local, dt, fluxes, &
 
 ! Joe
   if (CS%use_topodrag) then
-!    if (CS%debug) then
-!     call MOM_state_chksum("After topo_drag u,v", u,v, h_av, dt,G )
-!!     call MOM_state_chksum("After topo_drag u,v", u,v, h_av, dt,G, & 
-!!                     G%t11,G%t12,G%t21,G%t22, G%dragfac)
-!    endif
+   if (CS%debug) then
+    call MOM_state_chksum("After topo_drag u,v", u,v,h_av,uh,vh,G)
+   endif
   call topo_drag(u, v, h_av, dt, G,G%t11,G%t12,G%t21,G%t22, G%dragfac)
   call pass_vector(u, v, G%Domain)
-!    if (CS%debug) then
-!     call MOM_state_chksum("After topo_drag u,v", u,v, h_av, dt,G )
-!!     call MOM_state_chksum("After topo_drag u,v", u,v, h_av, dt,G, & 
-!!                     G%t11,G%t12,G%t21,G%t22, G%dragfac)
-!    endif
+   if (CS%debug) then
+    call MOM_state_chksum("After topo_drag u,v", u,v,h_av,uh,vh,G)
+   endif
   endif
 ! endJoe 
 
